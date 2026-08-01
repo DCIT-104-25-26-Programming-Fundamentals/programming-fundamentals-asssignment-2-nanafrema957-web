d126eb5b4183fd13bf89e7118601cc40903b9b07
nanafrema957-web <nanafrema957@gmail.com>
2026-08-01 15:43:01 +0000

    added main function

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -42,6 +42,7 @@
 #include <iostream>
 using namespace std;
 
+const int MAX_SIZE = 100;
 // Function to calculate the sum of all elements
 int calculateSum(int arr[], int n) {
     int sum = 0;
@@ -78,3 +79,29 @@ int findMin(int arr[], int n) {
     return min;
 }
 
+int main() {
+    int n;
+
+    cout << "How many numbers? ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Error: Please enter a positive number of values." << endl;
+        return 0;
+    }
+
+    int numbers[MAX_SIZE];
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter number " << (i + 1) << ": ";
+        cin >> numbers[i];
+    }
+
+    cout << "\nResults:" << endl;
+    cout << "Sum:     " << calculateSum(numbers, n) << endl;
+    cout << "Average: " << calculateAverage(numbers, n) << endl;
+    cout << "Maximum: " << findMax(numbers, n) << endl;
+    cout << "Minimum: " << findMin(numbers, n) << endl;
+
+    return 0;
+}
